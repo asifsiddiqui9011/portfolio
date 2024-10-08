982eac2dbcfa5ded9b9caac440ad63f1a27f1b48
Siddiqui Asif Ahmed <157271879+asifsiddiqui9011@users.noreply.github.com>
2024-10-08 11:15:02 +0530

    updated responsivness

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,18 +5,18 @@ import { FaRegFilePdf } from "react-icons/fa";
 const Intro = () => {
   return (
     <section id="about">
-    <div className="intro-container" >
-    <div className="about-container">
-      <h1>ABOUT</h1>
-      <p>
-      I am a Full Stack Developer with a background in Computer Science Engineering, specializing in the MERN stack (MongoDB, Express.js, React.js, Node.js). I have experience building dynamic, scalable web applications and delivering clean, efficient code. My expertise covers both front-end and back-end development, with a focus on creating user-friendly interfaces and robust APIs. I enjoy solving complex problems and optimizing performance to ensure seamless user experiences. Always eager to learn, I stay up-to-date with emerging technologies and thrive in collaborative, agile environments.
-      </p>
-    </div>
-    <div  className="about-container" >
-      <a href={'../../../public/ee.pdf'} download style={{textDecoration:"none", color:"black" , border:"3px solid white", padding:"20px", textAlign:"center", borderRadius:"15px"}}> <h2>Download Resume</h2> <FaRegFilePdf style={{fontSize:"100px",color:"black"}} />
-      </a> 
-    </div>
-    </div>
+      <div className="intro-container" >
+          <div className="about-container">
+            <h1>ABOUT</h1>
+            <p>
+            I am a Full Stack Developer with a background in Computer Science Engineering, specializing in the MERN stack (MongoDB, Express.js, React.js, Node.js). I have experience building dynamic, scalable web applications and delivering clean, efficient code. My expertise covers both front-end and back-end development, with a focus on creating user-friendly interfaces and robust APIs. I enjoy solving complex problems and optimizing performance to ensure seamless user experiences. Always eager to learn, I stay up-to-date with emerging technologies and thrive in collaborative, agile environments.
+            </p>
+          </div>
+          <div  className="about-container" >
+            <a href={'../../../public/ee.pdf'} download style={{textDecoration:"none", color:"black" , border:"3px solid white", padding:"20px", textAlign:"center", borderRadius:"15px"}}> <h2>Download Resume</h2> <FaRegFilePdf style={{fontSize:"100px",color:"black"}} />
+            </a> 
+          </div>
+      </div>
     </section>
   )
 }
